1e0eae0375f70f60f3fb4b8ab5f12734861a3c11
Carlos Borja <carlossocegadocruz@gmail.com>
2021-04-29 19:27:50 +0100

    Commit Inicial

--- app/src/main/java/com/example/dadospessoais/MainActivity.kt
+++ app/src/main/java/com/example/dadospessoais/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.dadospessoais
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +14,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun EnviarMensagem(view: View) {
+        val nome = findViewById<TextInputLayout>(R.id.textInputLayoutNome)
+        val email = findViewById<TextInputLayout>(R.id.textInputLayoutEmail)
+        val telefone = findViewById<TextInputLayout>(R.id.textInputLayoutTelefone)
+        val idade = findViewById<TextInputLayout>(R.id.textInputLayoutIdade)
 
+        val intent = Intent(this, MostrarMensagem::class.java)
+           startActivity(intent)
 
     }
 }

beddeadc2e3009a1d4ef65cc5789ea515244dcdd
Carlos Cruz <carlossocegadocruz@gmail.com>
2021-05-05 12:12:16 +0100

    Dados enviados

--- app/src/main/java/com/example/dadospessoais/MainActivity.kt
+++ app/src/main/java/com/example/dadospessoais/MainActivity.kt
@@ -14,13 +14,56 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun EnviarMensagem(view: View) {
-        val nome = findViewById<TextInputLayout>(R.id.textInputLayoutNome)
-        val email = findViewById<TextInputLayout>(R.id.textInputLayoutEmail)
-        val telefone = findViewById<TextInputLayout>(R.id.textInputLayoutTelefone)
-        val idade = findViewById<TextInputLayout>(R.id.textInputLayoutIdade)
+        val editTextNome = findViewById<EditText>(R.id.editTextNome)
+        val editTextEmail = findViewById<EditText>(R.id.editTextEmail)
+        val editTextTelefone = findViewById<EditText>(R.id.editTextTelefone)
+        val editTextIdade = findViewById<EditText>(R.id.editTextIdade)
 
-        val intent = Intent(this, MostrarMensagem::class.java)
-           startActivity(intent)
+        val nome = editTextNome.text.toString()
+        val email = editTextEmail.text.toString()
+        val telefone = editTextTelefone.text.toString()
+        val idade = editTextIdade.text.toString().toIntOrNull()
 
+        var dadosCorreto = true
+
+        if(nome.isBlank()){
+            editTextNome.setError(getString(R.string.nome_obrigatorio))
+            dadosCorreto = false
+        }
+
+        if(email.isBlank()){
+            editTextNome.setError(getString(R.string.email_obrigatorio))
+            dadosCorreto = false
+        }
+
+        if(telefone==null){
+            editTextEmail.setError(getString(R.string.telefone_obrigatorio))
+            dadosCorreto = false
+        }
+
+        if(idade==null){
+            editTextNome.setError(getString(R.string.idade_obrigatorio))
+            dadosCorreto = false
+        }
+
+        if(dadosCorreto) {
+
+            val intent = Intent(this, MostrarMensagem::class.java).apply {
+                putExtra(Companion.INFO_EXTRA_NOME, nome)
+                putExtra(Companion.INFO_EXTRA_EMAIL, email)
+                putExtra(Companion.INFO_EXTRA_TELEFONE, telefone)
+                putExtra(Companion.INFO_EXTRA_IDADE, idade)
+            }
+
+            startActivity(intent)
+        }
+
+    }
+
+    companion object {
+        const val INFO_EXTRA_NOME = "Nome"
+        const val INFO_EXTRA_EMAIL = "Email"
+        const val INFO_EXTRA_TELEFONE = "Telefone"
+        const val INFO_EXTRA_IDADE = "Idade"
     }
 }
--- app/src/main/java/com/example/dadospessoais/MostrarMensagem.kt
+++ app/src/main/java/com/example/dadospessoais/MostrarMensagem.kt
@@ -2,10 +2,21 @@ package com.example.dadospessoais
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MostrarMensagem : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostrar_mensagem)
+
+        val nome = intent.getStringExtra(MainActivity.INFO_EXTRA_NOME)
+        val email = intent.getStringExtra(MainActivity.INFO_EXTRA_EMAIL)
+        val telefone = intent.getStringExtra(MainActivity.INFO_EXTRA_TELEFONE)
+        val idade = intent.getIntExtra(MainActivity.INFO_EXTRA_IDADE, 0)
+
+         findViewById<TextView>(R.id.textViewNome).text = nome
+         findViewById<TextView>(R.id.textViewEmail).text = email
+        findViewById<TextView>(R.id.textViewTelefone).text = telefone
+        findViewById<TextView>(R.id.textViewIdade).text = idade.toString()
     }
 }
